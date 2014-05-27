8ea6025294b01fd63aa5653eb04dce0ebd351eb5
kevkev97kk <kevin97kang@gmail.com>
2014-05-27 00:05:55 -0700

    Made Changes in Wumpus class Requested by Isabelle. A rough sketch on requested method. Check if okay. Last Change of the day~ sorry.

--- WumpusTest/Wumpus.cs
+++ WumpusTest/Wumpus.cs
@@ -6,15 +6,18 @@ namespace WumpusTest
 {
     class Wumpus
     {
-        private Sound _sound;
+        //I WANT YOU NOT TO WORRY ABOUT MOVING THE WUMPUS BUT
+        //ALLOWING OTHERS TO KNOW WHEN WUMPUS IS ALLOWED TO MOVE
+        private Sound _sound; //GC WILL MAKE SOUNDS FOR YOU
         public enum WumpusState { asleep, awake, moving };
-        public WumpusState State { get; set; }
+        public WumpusState State { get; set; } //why do we need this?
 
         public enum numTurnsActiveWump { noMove, moveOnce, moveTwice, randomMove };
         public numTurnsActiveWump numTurns { get; set; }
 
         private Random random = new Random();
-        private int turns;
+        private int turns = 0;
+        private int subdivisionTurn = 0;
         private Map loc = new Map();
 
         public Wumpus()
@@ -49,11 +52,44 @@ namespace WumpusTest
             }
 
         }
+        /// <summary>
+        /// Decides whether the wumpus should move or not
+        /// </summary>
+        /// <returns>if it returns:
+        ///"teleport" - means map should teleport wumpus.
+        ///a string with integers - amount of moves before sleeping wumpus
+        ///"no" - do not move wumpus</returns>
+        public String howWumpusWillMove()
+        {
+            if (random.Next(100) < 5)
+            {
+                subdivisionTurn = 0;
+                return "teleport";
+            }
+            else if (subdivisionTurn >= 5 && subdivisionTurn < 10)
+            {
+                if (random.Next(50) < 10)
+                {
+                    subdivisionTurn = 0;
+                    int x = random.Next(3);
+                    return x.ToString();
+                }
+            }
+            else if (subdivisionTurn == 10)
+            {
+                subdivisionTurn = 0;
+                int x = random.Next(3);
+                return x.ToString();
+            }
+            //Trivia will be taken care of GC. Remind GameControl
+            return "no";
+        }
 
         //ADDED FOR CONVENIENCE
         public void addTurn()
         {
             turns++;
+            subdivisionTurn++;
         }
 
         // turn : turn++ when user asks if the wumpus should move. Enum of Active wumpus and lazy wumpus.
